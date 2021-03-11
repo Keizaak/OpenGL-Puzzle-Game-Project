6ac3d6ee2844c4bece7821150c5cbd0d60de4807
Arnaud <arnaud.rio@ecole.ensicaen.fr>
2021-03-11 11:09:44 +0100

    Define anchor and implementation of rotation of a piece

--- game/Asset_Designs/SimpleApp.cpp
+++ game/Asset_Designs/SimpleApp.cpp
@@ -47,13 +47,13 @@ SimpleApp::SimpleApp(int windowWidth, int windowHeight)
   glm::mat4 model1 = glm::scale(glm::mat4(1.), glm::vec3(1.1, 1.1, 1.1));
   model1 = glm::translate(model1, glm::vec3(0.5, 0.5, 0));
 
-  glm::vec2 anchor = positionsSmallRod[0];
+  glm::vec2 anchor = positionsCorner[0];
 
   glm::mat4 model2 = createRotationAroundAnchor(anchor,angle);
 //  model2 = glm::scale(model2, glm::vec3(1.1, 1.1, 1.1));
 
   // Le vecteur de rotation doit se situer sur le centre
-  makeA2DShape(positionsCorner,yellow,iboCorner,model0);
+  makeA2DShape(positionsCorner,yellow,iboCorner,model2);
   makeA2DShape(positionsRod,red,iboRod,model0);
   makeA2DShape(positionsSmallRod,blue,iboSmallRod,model0);
 }
